7332bda27c772755df2b513198d219ed3c413db6
harneet90 <contactharneet@gmail.com>
2020-07-01 20:24:26 +0530

    Initial

---
+++ include/hvector.h
@@ -0,0 +1,71 @@
+#pragma once
+template<class T>
+class HVector
+{
+    T *data;
+    size_t size;
+    size_t buff_size;
+    public:
+    HVector():data(nullptr), size(0), buff_size(0){}
+    HVector(size_t size):size(size), buff_size(size)
+    {
+        data = (T*)new uint8_t[sizeof(T)*size];
+    }
+    HVector(const HVector &other)
+    {
+        size = other.size;
+        buff_size= other.buff_size;
+        data = (T*)new uint8_t[sizeof(T)*buff_size];
+        for(size_t i=0;i<other.size;i++)
+        {
+             new(&data[i]) T(other.data[i]);
+        }
+    }
+    HVector& operator=(const HVector &other)
+    {
+        if(this == &other)
+             return *this;
+        for(size_t i=0;i < size;i++)
+         {
+	     data[i].~T();
+         }
+         delete[] (uint8_t*)data;
+        size = other.size;
+        buff_size= other.buff_size;
+        data = new T[buff_size];
+        for(size_t i=0;i<other.size;i++)
+        {
+             data[i] = other.data[i];
+        }
+        return *this;
+    }
+    void push_back(T &obj)
+    {
+         if(size == buff_size)
+         {
+              if(buff_size== 0)
+		buff_size= 1;
+	      else
+		buff_size*= 2;
+              T *temp= (T*)new uint8_t(sizeof(T) * buff_size);
+	      for(size_t i=0;i < size;i++)
+              {
+                 new(&temp[i]) T(data[i]);
+		 data[i].~T();
+              }
+              delete[] (uint8_t*)data;
+              data = temp;
+              data[size++] = obj;              
+         }
+         else
+	     data[size++] = obj;
+    }
+    virtual ~HVector(){ 
+         for(size_t i=0;i < size;i++)
+         {
+	     data[i].~T();
+         }
+         delete[] (uint8_t*)data;
+    }
+    size_t capacity(){return buff_size;}
+};
