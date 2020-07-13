26628e7ebc1fcc70513967dee070b138794f4791
harneet90 <contactharneet@gmail.com>
2020-07-13 22:24:43 +0530

    Create TestHVector.cpp

---
+++ tests/UnitTest/TestHVector.cpp
@@ -0,0 +1,22 @@
+#include "HVector.h"
+#include "UnitTest.h"
+
+void TestHVector_cons()
+{
+    BEGIN_TEST_CASE("HVector_cons");
+    HVector<int> x;
+    CHECK_RESULT(x.size(),0);
+    CHECK_RESULT(x.capacity(),0);
+    HVector<int> y(3);
+    CHECK_RESULT(x.size(),0);
+    CHECK_RESULT(x.capacity(),0);
+    END_TEST_CASE();
+}
+void TestHVector()
+{
+    BEGIN_TEST("HVector");
+    TestHVector_cons();
+    TestHVector_push_back();
+    TestHVector_pop_back();
+    END_TEST();
+}
