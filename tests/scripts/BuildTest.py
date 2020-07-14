35d98458928d2acfd2e908ecbf48ab945825275f
harneet90 <contactharneet@gmail.com>
2020-07-14 20:17:54 +0530

    Create BuildTest.py

---
+++ tests/scripts/BuildTest.py
@@ -0,0 +1,18 @@
+import os
+
+def BuildUnitTest():
+    print("Building Unit Test...")
+    os.chdir(os.path.join(os.getcwd(),os.pardir,"UnitTest"))
+    os.system("make -f makefile.mak clean")
+    os.system("make -f makefile.mak")
+    os.system("make -f makefile.mak install")
+    if (os.path.isfile(os.path.join(os.getcwd(),os.pardir,os.pardir,"bin","UnitTest")):
+        print("Build Failed")
+    else:
+        print("Build Successful")
+        
+def main():
+    BuildUnitTest()
+   
+if __name__ == '__main__':
+    main()
