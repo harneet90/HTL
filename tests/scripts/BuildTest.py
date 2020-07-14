7040715589c0b97247575a223b73a18b89907cf9
harneet90 <contactharneet@gmail.com>
2020-07-14 22:57:44 +0530

    Update BuildTest.py

--- tests/scripts/BuildTest.py
+++ tests/scripts/BuildTest.py
@@ -6,7 +6,7 @@ def BuildUnitTest():
     os.system("make -f makefile.mak clean")
     os.system("make -f makefile.mak")
     os.system("make -f makefile.mak install")
-    if (!os.path.isfile(os.path.join(os.getcwd(),os.pardir,os.pardir,"bin","UnitTest"))):
+    if (not os.path.isfile(os.path.join(os.getcwd(),os.pardir,os.pardir,"bin","UnitTest"))):
         print("Build Failed")
     else:
         print("Build Successful")
