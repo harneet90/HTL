bd80507a89e07da5669ffab3bee87f9fddaaf597
harneet90 <contactharneet@gmail.com>
2020-07-14 22:38:52 +0530

    Update TestHVector.cpp

--- tests/UnitTest/TestHVector.cpp
+++ tests/UnitTest/TestHVector.cpp
@@ -5,11 +5,11 @@ void TestHVector_cons()
 {
     BEGIN_TEST_CASE("HVector_cons");
     HVector<int> x;
-    CHECK_RESULT(x.size(),0);
-    CHECK_RESULT(x.capacity(),0);
+    CHECK_RESULT(x.size()!=0);
+    CHECK_RESULT(x.capacity()!=0);
     HVector<int> y(3);
-    CHECK_RESULT(x.size(),0);
-    CHECK_RESULT(x.capacity(),0);
+    CHECK_RESULT(x.size()!=0);
+    CHECK_RESULT(x.capacity()!=0);
     END_TEST_CASE();
 }
 
