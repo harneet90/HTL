61357af66eff21d8f84f618b2223a95ac01932a0
harneet90 <contactharneet@gmail.com>
2020-07-15 20:40:00 +0530

    Update UnitTest.cpp

--- tests/UnitTest/UnitTest.cpp
+++ tests/UnitTest/UnitTest.cpp
@@ -4,5 +4,8 @@ int total_pass=0;
 int main()
 {
     TestHVector();
+    cout<<"Total Passed: "<<total_pass<<"/"<<grand_total<<endl;
+    if(grand_total != total_pass)
+        return 1;
     return 0;
 }
