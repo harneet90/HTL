0be894016d627c35cd4ba2bc6685838b5bc09856
harneet90 <contactharneet@gmail.com>
2020-07-20 21:08:40 +0530

    Update TestHVector.cpp

--- tests/UnitTest/TestHVector.cpp
+++ tests/UnitTest/TestHVector.cpp
@@ -4,43 +4,77 @@
 void TestHVector_cons()
 {
     BEGIN_TEST_CASE("HVector_cons");
+    /* Default Constructor */
     HVector<int> x;
-    CHECK_RESULT(x.size()!=0);
-    CHECK_RESULT(x.capacity()!=0);
+    CHECK_RESULT(x.size() != 0);
+    CHECK_RESULT(x.capacity() != 0);
+    /* Size Construcotr */
     HVector<Test> y(3);
-    CHECK_RESULT(y.size()!=3);
-    CHECK_RESULT(y.capacity()!=3);
+    CHECK_RESULT(y.size() != 3);
+    CHECK_RESULT(y.capacity() != 3);
     CHECK_RESULT(cons_call != 3);
+    /* Fill Constructor */
+    HVector<int> z(4,4);
+    CHECK_RESULT(y.size() != 4);
+    for(int i=0;i<4;i++)
+        CHECK_RESULT(z[i] != 4);
+    /* Initializer List Construcotr */
+    HVector<int> w{1,2,3,4,5};
+    CHECK_RESULT(y.size() != 5);
+    CHECK_RESULT(y.capacity() != 5);
+    for(int i=i;i<=5;i++)
+        CHECK_RESULT(w[i-1] != i);
     END_TEST_CASE();
 }
 
-void TestHVector_push_back()
+void TestHVector_push_pop()
 {
     BEGIN_TEST_CASE("HVector_push_back");
     HVector<int> x;
     x.push_back(5);
-    CHECK_RESULT(x.size()!=1);
-    CHECK_RESULT(x.capacity()!=1);
+    CHECK_RESULT(x.size() != 1);
+    CHECK_RESULT(x.capacity() != 1);
     x.push_back(6);
-    CHECK_RESULT(x.size()!=2);
-    CHECK_RESULT(x.capacity()!=2);
+    CHECK_RESULT(x.size() != 2);
+    CHECK_RESULT(x.capacity() != 2);
     x.push_back(7);
-    CHECK_RESULT(x.size()!=3);
-    CHECK_RESULT(x.capacity()!=4);
+    CHECK_RESULT(x.size() != 3);
+    CHECK_RESULT(x.capacity() != 4);
     for(int i=0;i<3;i++)
         CHECK_RESULT(x[i]!= 5+i);
     HVector<Test> y;
+    Test t;
+    y.push_back(t);
     y.push_back(Test());
-    CHECK_RESULT(cons_call!=1);
-    CHECK_RESULT(copy_cons_call!=1);
+    CHECK_RESULT(cons_call != 2);
+    CHECK_RESULT(copy_cons_call != 1);
+    CHECK_RESULT(move_cons_call != 1);
+    y.pop_back();
+    CHECK_RESULT(des_call != 1);
+    CHECK_RESULT(x.size() != 1);
+    CHECK_RESULT(x.capacity() != 2);
     END_TEST_CASE();
+}
+
+void TestHVector_begin_end()
+{
+    HVector<int> x;
+    x.push_back(5);
+    CHECK_RESULT(*(x.begin()) != 5);
+    CHECK_RESULT(x.begin() != x.end());
+    x.push_back(6);
+    CHECK_RESULT(*(x.end()) != 6);
+    CHECK_RESULT(x.begin() + 1 != x.end());
+    int i=0;
+    for(auto itr = x.begin();itr != x.end();itr++,i++)
+        CHECK_RESULT(*itr != 5 + i);
 }
 
 void TestHVector()
 {
     BEGIN_TEST("HVector");
     TestHVector_cons();
-    TestHVector_push_back();
-    //TestHVector_pop_back();
+    TestHVector_push_pop();
+    TestHVector_begin_end();
     END_TEST();
 }
