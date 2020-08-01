f7139eccb4bfc0e7f3a389d18300daa3a31097fa
harneet90 <contactharneet@gmail.com>
2020-08-01 21:47:26 +0530

    Add lookup and delete. Made it generic.

--- include/util/HRBTree.h
+++ include/util/HRBTree.h
@@ -1,33 +1,149 @@
 #include<iostream>
 using namespace std;
-
+template<typename T>
 class HAVL{
-    public:
+    private:
       struct node{
       node* left;
       node* right;
       node* parent;
-      int data;
-      node(int data):data(data),left(nullptr),right(nullptr),parent(nullptr){}
+      const T* data;
+      node(const T& data):data(new T(data)),left(nullptr),right(nullptr),parent(nullptr){}
   } ; 
     node* root;
+    void _printTree(node *root);
+    bool _lookup(const T &ele, node* root,  node *&parent)
+    {
+        if(root == nullptr)
+            return false;
+        if(*(root->data) == ele)
+        {
+            return true;
+        }
+        if(*(root->data) < ele)
+        {
+            if(root->right)
+            {
+                if(*(root->right->data) == ele)
+                {
+                    parent = root;
+                    return true;
+                }
+                else
+                {
+                    return _lookup(ele, root->right, parent);
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if(root->left) 
+            {
+                if(*(root->left->data) == ele)
+                {
+                    parent = root;
+                    return true;
+                }
+                else
+                {
+                    return _lookup(ele, root->left, parent);
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }   
+    }
+    public:
     HAVL():root(nullptr){}
-    node* add(int ele){insertBST(root,new node(ele)); return root;}
-    node* getRoot() { return root;}
-    void printTree(node *root);
- 
+    node* add(const T &ele){insertBST(root,new node(ele)); return root;}
+    bool lookup(const T &ele){node *parent; return _lookup(ele,root,parent);}
+    void remove(const T &ele);
+    void printTree(){_printTree(root);}
+    
   bool insertBST(node *&root, node *t);
 
 };
-void HAVL::printTree(node *root)
+template<typename T>
+void HAVL<T>::remove(const T &ele)
+{
+    node *parent = NULL;
+    if(_lookup(ele,root,parent))
+    {
+        node *N = root, *temp = NULL;
+        bool left = false;
+        if(parent)
+        {
+            if(parent->left  && ele == *(parent->left->data))
+            {
+                N = parent->left;
+                left = true;
+            }
+            else
+            {
+                N = parent->right;
+            }
+        }
+        if(!N->left)
+        {
+            temp = N->right;
+        }
+        else if(!N->right)
+        {
+            temp = N->left;
+        }
+        else
+        {
+            node *t1, *t2;
+            t1 = N;
+            t2 = t1->right;
+            if(t2->left != nullptr)
+            {
+                while(t2->left != nullptr)
+                {
+                    t1 = t2;
+                    t2 = t2->left;
+                }
+                t1->left = t2->right;
+                t2->right = N->right;
+            }
+            t2->left = N->left;
+            temp = t2;
+        }
+        if(parent)
+        {
+            if(left)
+            {
+                parent->left = temp;
+            }
+            else
+            {
+                parent->right = temp;
+            }
+        }
+        else
+        {
+            root = temp;
+        }
+        delete N;
+    }
+}
+template<typename T>
+void HAVL<T>::_printTree(node *root)
 {
     if(root == NULL)
         return;
-    printTree(root->left);
-    cout<<root->data<<" -> ";
-    printTree(root->right);
+    _printTree(root->left);
+    cout<<*(root->data)<<" -> ";
+    _printTree(root->right);
 }
-bool HAVL::insertBST(node *&root, node *t)
+template<typename T>
+bool HAVL<T>::insertBST(node *&root, node *t)
 {
     if(root == NULL)
     {
@@ -38,11 +154,11 @@ bool HAVL::insertBST(node *&root, node *t)
     while(p != nullptr)
     {
         g = p;
-        if(p->data == t->data)
+        if(*(p->data) == *(t->data))
         {
             return false;
         }
-        if(p->data > t->data)
+        if(*(p->data) > *(t->data))
         {
             p = p->left;
         }
@@ -51,7 +167,7 @@ bool HAVL::insertBST(node *&root, node *t)
             p = p->right;
         }
     }
-    if(g->data > t->data)
+    if(*(g->data) > *(t->data))
         g->left = t;
     else
         g->right = t;
